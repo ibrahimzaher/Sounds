977a0968e5d6f3438db0dd91f466133450266841
ibrahimzaher <hemazaher15@gmail.com>
2026-05-15 04:48:03 +0300

    feat: update remote registration logic to use enhanced runtime and local port mapping

--- apps/E-commerce/src/main.ts
+++ apps/E-commerce/src/main.ts
@@ -1,9 +1,27 @@
-import { setRemoteDefinitions } from '@nx/angular/mf';
+import { registerRemotes } from '@module-federation/enhanced/runtime';
+
+// خريطة لربط اللينكات بالـ Ports المحلية
+const LOCAL_PORTS: Record<string, number> = {
+  dashboard: 4201,
+};
 
 fetch('/module-federation.manifest.json')
   .then((res) => res.json())
-  .then((definitions) => {
-    setRemoteDefinitions(definitions);
+  .then((remotes: Record<string, string>) => {
+    const isLocal = window.location.hostname === 'localhost';
+
+    return Object.entries(remotes).map(([name, entry]) => {
+      let finalEntry = entry;
+
+      if (isLocal && LOCAL_PORTS[name]) {
+        finalEntry = `http://localhost:${LOCAL_PORTS[name]}/mf-manifest.json`;
+      }
+
+      return { name, entry: finalEntry };
+    });
   })
-  .then(() => import('./bootstrap'))
-  .catch((err) => console.error(err));
+  .then((remotes) => {
+    console.log('Registered Remotes:', remotes);
+    return registerRemotes(remotes);
+  })
+  .then(() => import('./bootstrap').catch((err) => console.error(err)));
